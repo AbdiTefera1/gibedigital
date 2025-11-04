1cea506e7818666f3aed64a5eef434c8be6b90ed
Abdi Tefera <abditefera11@gmail.com>
2025-11-03 23:18:20 -0800

    some part of website is updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,21 @@ export const metadata: Metadata = {
   icons: {
     icon: "/gibe.png",
   },
+  openGraph: {
+    images: "/gibe.png",
+    title: "Gibe Digital | We Design, You Shine",
+    description: "Gibe Digital is a creative tech studio that blends design and technology to help businesses shine. We offer branding, web development, UI/UX, social media management, video editing, and digital marketing services.",
+    url: "https://gibe.digital",
+    siteName: "Gibe Digital",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Gibe Digital | We Design, You Shine",
+    description: "Gibe Digital is a creative tech studio that blends design and technology to help businesses shine. We offer branding, web development, UI/UX, social media management, video editing, and digital marketing services.",
+    images: "/gibe.png",
+  },
 };
 
 export default function RootLayout({
