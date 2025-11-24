7edc34c6e595011d1c65ecf3fcade288403e8a7a
Abdi Tefera <abditefera11@gmail.com>
2025-11-24 05:34:11 -0800

    I updated package.json

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,7 @@
-// import { PrismaClient } from "@/app/generated/prisma/client"
-// import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
-
-// const adapter = new PrismaBetterSqlite3({
-//   url: process.env.DATABASE_URL ?? '',
-// });
-
-// const prisma = new PrismaClient({ adapter });
-
-// export default prisma;
-
-import { PrismaClient } from '../app/generated/prisma/client'
+// ./lib/prisma.ts
+import { PrismaClient } from '../app/generated/prisma/client'  // Relative path from lib/ to app/
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
-
-export default prisma;
+export default prisma
