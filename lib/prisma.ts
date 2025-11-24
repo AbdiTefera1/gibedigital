bb6e5b5a6254287fd86372a1b58546eadf3779ab
Abdi Tefera <abditefera11@gmail.com>
2025-11-24 05:12:48 -0800

    I updated package.json

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,7 +9,7 @@
 
 // export default prisma;
 
-import { PrismaClient } from '@/app/generated/prisma/client'
+import { PrismaClient } from '../app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
