d815a374636ad9b7bb45fee66179d665d8c2b462
Abdi Tefera <abditefera11@gmail.com>
2025-11-25 07:27:42 -0800

    I made some update

--- lib/permissionGuard.ts
+++ lib/permissionGuard.ts
@@ -2,16 +2,25 @@
 import { JWTPayload } from "@/lib/jwt";
 import { RolePermissions } from "@/lib/rbac";
 
-export function permissionGuard(
-  user: JWTPayload | null,
-  requiredPermission: string
-): boolean {
-  if (!user) return false;
+// export function permissionGuard(
+//   user: JWTPayload | null,
+//   requiredPermission: string
+// ): boolean {
+//   if (!user) return false;
 
-  const role = user.role ?? "";
-  const permissions = RolePermissions[role];
+//   const role = user.role ?? "";
+//   const permissions = RolePermissions[role];
 
-  if (!permissions) return false;
+//   if (!permissions) return false;
 
-  return permissions.includes(requiredPermission);
-}
+//   return permissions.includes(requiredPermission);
+// }
+
+export function permissionGuard(user: JWTPayload | null, requiredPermission: string) {
+    if (!user) return false;
+  
+    const rolePermissions = RolePermissions[user.role ?? ""] ?? [];
+    const userPermissions = user.permissions ?? [];
+  
+    return [...rolePermissions, ...userPermissions].includes(requiredPermission);
+  }
